4389ad11186ddc7d906aacd2be8138513e44f9e9
Don-Patey <139824991+Don-Patey@users.noreply.github.com>
2024-01-02 11:52:37 -0400

    started filling out routes

---
+++ models/thought.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String, 
+        required: true, 
+        unique: true, 
+        trim: true, 
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    username: {
+        type: String, 
+        required: true, 
+    },
+    reactions: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Reaction'
+        }
+    ]
+});
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
